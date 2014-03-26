edd88b097994329acea2e8fc815a6a27ab03fd71
DveMac <david@milkjar.co.uk>
2014-03-26 10:56:36 +0000

    Added: crappy hash query

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,70 +1,61 @@
 /** @jsx React.DOM */
+(function () {
+  var Tag = React.createClass({
+    render: function () {
+      return <a className="tag" onClick={this.props.setQuery.bind(null, this.props.name)}>{ this.props.name }</a>
+    }
+  });
 
-var Tag = React.createClass({
-  render: function () {
-    return <a className="tag" onClick={this.props.setQuery.bind(null, this.props.name)}>{ this.props.name }</a>
-  }
-});
+  var Item = React.createClass({
+    displayName: 'Item',
+    render: function () {
+      var tags = [], _this = this;
+      d3.map([this.props.proj.group].concat(this.props.proj.keywords||[])).forEach(function (k, v) {
+        if (v !== 'react' && v !== 'react-component') {
+          tags.push(<Tag setQuery={_this.props.setQuery} key={k} name={v} />);
+        }
+      });
+      return <li className="project">
+          <a href={ this.props.proj.repo }>{ this.props.proj.name }</a><br/>
+          <i className="">{ this.props.proj.description }</i><br/>
+          <small className="">created { moment(this.props.proj.created).format("MMM Do YY") },  modified { moment(this.props.proj.modified).fromNow() }</small><br/>
+          {tags}
+      </li>
+    }
+  });
 
-var Item = React.createClass({
-  displayName: 'Item',
-  render: function () {
-    var tags = [], _this = this;
-    d3.map(this.props.proj.keywords).forEach(function (k, v) {
-      if (v !== 'react' && v !== 'react-component') {
-        tags.push(<Tag setQuery={_this.props.setQuery} key={k} name={v} />);
-      }
-    });
-    return <li className="project">
-        <a href={ this.props.proj.repo }>{ this.props.proj.name }</a><br/>
-        <i className="">{ this.props.proj.description }</i><br/>
-        <small className="">{ this.props.proj.modified }</small><br/>
-        {tags}
-    </li>
-  }
-});
+  var Cats = React.createClass({
+    displayName: 'List',
+    getInitialState: function() {
+      return { query: null };
+    },
+    handleChange: function(event) {
+      this.setQuery(event.target.value);
+    },
+    setQuery: function (query) {
+      this.setState({ query: query });
+    },
+    render: function () {
+      var items = [], _this = this,
+        query = this.state.query === null ? this.props.query : this.state.query,
+      data = (this.props.data || []).filter(function (p) {
+        return query.length === 0 || (!!~p.name.indexOf(query)
+            || !!~p.group.indexOf(query)
+            || !!~(p.keywords || []).join().indexOf(query));
+      });
+      d3.map(data).forEach(function (k,v) {
+        items.push(<Item setQuery={_this.setQuery} key={k} proj={v} />)
+      });
+      var body = (<div><input type="text" className="search" value={query} onChange={this.handleChange}  placeholder="Enter a keyword or project name..." /><ul>{items}</ul></div>);
+      var loading = <h2>Loading...</h2>;
+      return this.props.data ? body : loading;
+    }
+  });
 
-var List = React.createClass({
-  displayName: 'List',
-  render: function () {
-    var items = [], query = this.props.query,
-    _this = this,
-    data = this.props.projects.filter(function (p) {
-      return !!~p.name.indexOf(query) || !!~(p.keywords || []).join().indexOf(query);
-    });
-    d3.map(data).forEach(function (k,v) {
-      items.push(<Item setQuery={_this.props.setQuery} key={k} proj={v} />)
-    });
-    return (<ul>{items}</ul>);
-  }
-});
+  var path = (window.document.location.hash || '').replace('#','');
+  var App = React.renderComponent(<Cats query={path} />, document.getElementById('main_content'));
 
-var Cats = React.createClass({
-  displayName: 'List',
-  getInitialState: function() {
-    return {value: ''};
-  },
-  handleChange: function(event) {
-    this.setQuery(event.target.value);
-  },
-  setQuery: function (query) {
-    this.setState({value: query});
-  },
-  render: function () {
-    var lists = [], _this = this, value = this.state.value;
-    d3.map(this.props.data).forEach(function (k, v) {
-      lists.push(<li key={k}><h3>{k}</h3><List setQuery={_this.setQuery} query={value} projects={v} /></li>)
-    });
-    var body = (<div><input type="text" className="search" value={value} onChange={this.handleChange}  placeholder="Enter a keyword or project name..." /><ul>{lists}</ul></div>);
-    var loading = <h2>Loading...</h2>;
-    return this.props.data ? body : loading;
-  }
-});
-
-var App = React.renderComponent(<Cats/>, document.getElementById('main_content'));
-
-
-d3.json('dist/list.json', function (data) {
-  App.setProps({ data: data });
-});
-
+  d3.json('dist/list.json', function (data) {
+    App.setProps({ data: data, query: path });
+  });
+}());
