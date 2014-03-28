91428eccf0cc0c59840220dcc79fd5aae95ec6cd
DveMac <david@milkjar.co.uk>
2014-03-28 10:46:03 +0000

    Missin file

--- dist/app.js
+++ dist/app.js
@@ -1,23 +1,24 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /** @jsx React.DOM */
 var React = require('react');
-var d3 = require('d3');
+var _ = require('lodash');
 var Item = require('./item.react');
 
 var resultCountStyle = { float: 'right', marginTop: '-20px', fontSize: '80%' };
 
 function getfilter(query) {
   return function (p) {
-    return query.length === 0 || (!!~p.name.indexOf(query)
-      || !!~p.group.indexOf(query)
-      || !!~(p.keywords || []).join().indexOf(query));
-  }
+    return query.length === 0 ||
+      (!!~p.name.indexOf(query) ||
+        !!~p.group.indexOf(query) ||
+        !!~(p.keywords || []).join().indexOf(query));
+  };
 }
 
 function getDateSort(field) {
   return function (a, b) {
     return new Date(b[field]).getTime() - new Date(a[field]).getTime();
-  }
+  };
 }
 
 module.exports = React.createClass({displayName: 'exports',
@@ -35,16 +36,14 @@ module.exports = React.createClass({displayName: 'exports',
   },
 
   render: function () {
-    var items = [],
-      query = this.state.query === null ? this.props.query : this.state.query,
-      sort = this.state.sort === null ? this.props.sort : this.state.sort,
-      data = (this.props.data || [])
-        .filter(getfilter(query))
-        .sort(getDateSort(sort));
+    var query = this.state.query === null ? this.props.query : this.state.query,
+      sort = this.state.sort === null ? this.props.sort : this.state.sort;
 
-    d3.map(data).forEach(function (k, v) {
-      items.push(Item( {key:k, proj:v} ));
-    });
+    var items = _(this.props.data || [])
+      .filter(getfilter(query))
+      .sort(getDateSort(sort))
+      .map(function (v, k) { return (Item( {key:k, proj:v} )); })
+      .value();
 
     return !this.props.data
       ? (React.DOM.h2(null, "Loading..."))
@@ -59,26 +58,33 @@ module.exports = React.createClass({displayName: 'exports',
   }
 
 });
-},{"./item.react":3,"d3":"Ub4Hh8","react":"M6d2gk"}],2:[function(require,module,exports){
+},{"./item.react":3,"lodash":"K2RcUv","react":"M6d2gk"}],2:[function(require,module,exports){
 var React = require('react');
-var d3 = require('d3');
 var App = require('./app.react');
 
 function getHash() {
-    return (window.document.location.hash || '').replace('#', '');
+  return (window.document.location.hash || '').replace('#', '');
 }
 
 var app = React.renderComponent(App({query: getHash()}), document.getElementById('main_content'));
 
 window.addEventListener('hashchange', function (hash) {
-    app.setProps({ query: getHash() });
+  app.setProps({ query: getHash() });
 });
 
-d3.json('dist/list.json', function (data) {
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function (ev) {
+  if (xhr.readyState === 4) {
+    var data = JSON.parse(ev.target.response);
     app.setProps({ data: data, query: getHash() });
-});
+  }
+};
 
-},{"./app.react":1,"d3":"Ub4Hh8","react":"M6d2gk"}],3:[function(require,module,exports){
+xhr.open("GET", 'dist/list.json', true);
+xhr.send(null);
+
+},{"./app.react":1,"react":"M6d2gk"}],3:[function(require,module,exports){
 /** @jsx React.DOM */
 var React = require('react');
 var moment = require('moment');
@@ -96,7 +102,8 @@ module.exports = React.createClass({
     var tags = _(keywords)
       .filter(function (v) { return !~blacklist.indexOf(v); })
       .uniq()
-      .map(function (v) { return (Tag( {key:v, name:v} )); });
+      .map(function (v) { return (Tag( {key:v, name:v} )); })
+      .value();
 
     return (React.DOM.li( {className:"project"}, 
       React.DOM.h3(null, 
